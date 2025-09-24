4f5edf650e823020082ff93401f88b4a83599fc2
Protyay Kumar Pramanik <88667795+propmk@users.noreply.github.com>
2025-09-25 01:30:00 +0600

    inserted demo data in the demo tables

--- init.sql
+++ init.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS customers (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL,
@@ -11,3 +11,13 @@ CREATE TABLE IF NOT EXISTS products (
     price DECIMAL(10, 2) NOT NULL,
     stock INT NOT NULL
 );
+
+INSERT INTO customers (username, email) VALUES
+    ('sakib', 'sakib@example.com'),
+    ('ragib', 'ragib@example.com'),
+    ('jamal', 'jamal@example.com');
+
+INSERT INTO products (name, price, stock) VALUES
+    ('Pen', 9.99, 70),
+    ('Pencil', 9.99, 60),
+    ('Calculator', 1000.00, 10);
